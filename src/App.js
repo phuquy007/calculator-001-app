3ac69412104b7328c59ce644c1d53070a904c601
Ryan Tran <tranp6@student.douglascollege.ca>
2021-03-09 16:28:59 -0800

    Added Skeleton Interface to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import CalculatorFormPtr18 from "./Components/CalculatorFormPtr18/CalculatorFormPtr18"
 
 function App() {
   return (
     <div className="App">
       <h1>This is a calculator</h1>
+      <CalculatorFormPtr18 />
     </div>
   );
 }
